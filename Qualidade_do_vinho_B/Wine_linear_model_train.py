ea2d4a1865583b6a7b3df37d4600a165bdf718a6
gomescarla <alvescarlasofia9@gmail.com>
2026-03-20 12:23:24 +0000

    carlaaa

--- Qualidade_do_vinho_B/Wine_linear_model_train.py
+++ Qualidade_do_vinho_B/Wine_linear_model_train.py
@@ -5,8 +5,7 @@ import pandas as pd
 from sklearn import datasets, linear_model
 from sklearn.metrics import mean_squared_error, r2_score
 
-data = pd.read_csv("C:\Users\alves\Downloads")
-sep=";")
+data = pd.read_csv("./winequality-white.csv",sep=";")
 
 train_data=data[:1000]
 data_X=train_data.iloc[:,0:11]
@@ -16,6 +15,12 @@ print(data_X)
 print(data_Y)
 regr = linear_model.LinearRegression()
 preditor_linear_model=regr.fit(data_X, data_Y)
-preditor_Pickle = open('../white-wine_quality_predictor', 'wb')
+preditor_Pickle = open('white-wine_quality_predictor', 'wb')
 print("white-wine_quality_predictor")
 p1.dump(preditor_linear_model, preditor_Pickle)
+
+
+
+
+
+
